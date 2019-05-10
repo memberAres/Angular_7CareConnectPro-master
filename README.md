be27a69a294dcfe030ed4e5db40b29714b18046d
member@res730 <BoyVit85@mail.ru>
2019-05-09 23:25:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Angular_7CareConnectPro-master
---
+++ projects/careconnectpro-web/src/app/components/shared/notification/notifications.component.ts
@@ -0,0 +1,54 @@
+﻿import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Message } from "primeng/primeng";
+import {MessageService} from 'primeng/api';
+import { NotificationsService } from "service-lib";
+import { Subscription } from "rxjs/Subscription";
+
+@Component({
+  selector: "app-notifications",
+  templateUrl: "./notifications.component.html",
+  styleUrls: ["./notifications.component.css"],
+  styles: [`
+        :host ::ng-deep button {
+            margin-right: .25em;
+        }
+
+        :host ::ng-deep .custom-toast .ui-toast-message {
+            color: #ffffff;
+            background: #FC466B;
+            background: -webkit-linear-gradient(to right, #3F5EFB, #FC466B);
+            background: linear-gradient(to right, #3F5EFB, #FC466B);
+        }
+
+        :host ::ng-deep .custom-toast .ui-toast-close-icon {
+            color: black;
+        }
+    `],
+  providers: [MessageService]
+})
+export class NotificationsComponent implements OnInit, OnDestroy {
+  msgs: Message[] = [];
+  subscription: Subscription;
+
+  constructor(
+    private notificationsService: NotificationsService,
+    private messageService: MessageService
+    ) {}
+
+  ngOnInit() {
+    this.subscribeToNotifications();
+  }
+
+  subscribeToNotifications() {
+    this.subscription = this.notificationsService.notificationChange.subscribe(
+      notification => {
+        this.msgs = [];
+        this.messageService.add(notification);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
